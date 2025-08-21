f10e0251975308da8b4832f2071e1a9f91810b8a
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2025-08-22 03:56:35 +1000

    Initial BepisLoader release

---
+++ Patch.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.NET.Common;
+using Elements.Core;
+using FrooxEngine;
+using FrooxEngine.UIX;
+using HarmonyLib;
+using Renderite.Shared;
+
+namespace DefaultLaserState;
+
+public abstract class BaseResonitePlugin : BasePlugin
+{
+    public abstract string Author { get; }
+    public abstract string Link { get; }
+}
+
+[BepInPlugin(GUID, Name, Version)]
+public class Patch : BaseResonitePlugin
+{
+    public const string GUID = "dev.lecloutpanda.defaultlaserstate";
+    public const string Name = "Default Laser State";
+    public const string Version = "1.0.0";
+    public override string Author => "LeCloutPanda";
+    public override string Link => "https://github.com/LeCloutPanda/DefaultLaserState";
+
+    private static ConfigEntry<bool> LASERSTATE;
+
+    public override void Load()
+    {
+        LASERSTATE = Config.Bind("General", "Default Laser State", true, "Enable/disable the laser on spawn");
+    
+        HarmonyInstance.PatchAll();
+    }
+
+	[HarmonyPatch(typeof(InteractionHandler), "OnAwake")]
+	private class CommonToolPatch
+	{
+		[HarmonyPostfix]
+		private static void Postfix(InteractionHandler __instance, Sync<bool> ____laserEnabled)
+		{
+			InteractionHandler __instance2 = __instance;
+			Sync<bool> ____laserEnabled2 = ____laserEnabled;
+			__instance2.RunInUpdates(3, delegate
+			{
+				if (__instance2.Slot.ActiveUser == __instance2.LocalUser)
+				{
+					____laserEnabled2.Value = LASERSTATE.Value;
+				}
+			});
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Context Menu Funnies
+A [Resonite](https://resonite.com/) mod that lets you change the look of your context menu.
+
+# BepisLoader Installation
+1. Install [BepisLoader](https://github.com/ResoniteModding/BepisLoader).
+2. Place [DefaulLaserState.dll](https://github.com/LeCloutPanda/DefaulLaserState/releases/latest/download/DefaulLaserState.dll) into your `plugins` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\BepInEx\plugins` for a default install.
+3. Start the game. If you want to verify that the mod is working you can check your Resonite logs. 
